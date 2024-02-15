7507edccb9599b8ff20c44b838b9aaf22327b56c
rabin-thami <rabinthami36@gmail.com>
2024-02-15 08:22:33 +0545

    design completed

---
+++ src/Components/Init.jsx
@@ -0,0 +1,16 @@
+
+
+const Init = () => {
+
+    return (
+        <section className="init-section">
+            <div className="init-container">
+                <h1>Quizzical</h1>
+                <p>Test your knowladge Here</p>
+                <button> Start </button>
+            </div>
+        </section>
+    )
+}
+
+export default Init
---
+++ src/Components/question.jsx
@@ -0,0 +1,33 @@
+
+
+const Question = () => {
+    return (
+        <section className="question-section">
+            <div className="question-container">
+                <h1>How would you say goodbye in Spanish ? </h1>
+                <div className="ans-container">
+                    <button className="active"> Adious </button>
+                    <button> Hola </button>
+                    <button> Goodbye </button>
+                    <button> FuckOff </button>
+                </div>
+                <hr className="bottom-line"/>
+            </div>
+            <div className="question-container">
+                <h1>How would you say goodbye in Spanish ? </h1>
+                <div className="ans-container">
+                    <button className="active"> Adious </button>
+                    <button> Hola </button>
+                    <button> Goodbye </button>
+                    <button> FuckOff </button>
+                </div>
+                <hr />
+
+                <button> Check Answer </button>
+            </div>
+        </section>
+
+    )
+}
+
+export default Question
